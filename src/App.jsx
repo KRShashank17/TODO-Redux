9baf89284705bcae0d9c94805fadca01be68bed9
Shashank K R <krshashank1118@gmail.com>
2024-02-13 21:59:18 +0530

    @reduxjs/toolkit react-redux Configure Store

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-blue-950 text-white text-2xl font-bold text-center h-screen flex justify-center items-center">
+      
+        <h2 className="font-bold text-center h-screen justify-center items-center">
+        HELLO
+        </h2>
+
+      </div>
+    </>
+  )
+}
+
+export default App
